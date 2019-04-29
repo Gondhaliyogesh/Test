3f0cbab65d55808ccdefd39230eb742749064fd1
Gondhaliyogesh <yogeshsgondhali@gmail.com>
2019-04-29 17:18:44 +0530

    Ten

--- src/test/java/com/Test/PageTest/HomepageTest.java
+++ src/test/java/com/Test/PageTest/HomepageTest.java
@@ -1,8 +1,12 @@
 package com.Test.PageTest;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -20,7 +24,7 @@ public class HomepageTest
 	Homepage homepage;
 
 	@BeforeSuite
-	public void Start()
+	public void Start() throws MalformedURLException
 	{
 		/*System.setProperty("webdriver.gecko.driver", "C:\\Users\\Admin\\Desktop\\Yogesh Gondhali\\Software\\geckodriver.exe");
 		driver=new FirefoxDriver();
@@ -39,6 +43,16 @@ public class HomepageTest
 	        options.setCapability("version", "73.0");
 	        options.setCapability("build", "Paradigm_Login_Function");
 	        options.setCapability("name", "Login_Page");
+	        
+	        driver = new RemoteWebDriver(new URL("http://ondemand.saucelabs.com:80/wd/hub"), options);
+
+	        /*System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Desktop\\Yogesh Gondhali\\Software\\chromedriver.exe");
+	        driver=new ChromeDriver();*/
+
+	        driver.manage().window().maximize();
+	        driver.manage().deleteAllCookies();
+
+	        driver.get("http://pcsv2dev.azurewebsites.net/#/");
 
 	}
 	
